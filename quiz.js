0b6313603d97f81e835f45ef40216b9bf2643f42
sf <sreedfenton@gmail.com>
2024-04-07 19:03:49 -0700

    upon studying today, i realized that the fix i put in to exclude the correct answer from the incorrect answer pool was not functioning properly.

--- quiz.js
+++ quiz.js
@@ -18,8 +18,8 @@ fs.readFile('lesson-3.txt', 'utf8', function (err, file) {
       englishWord = englishWord.trim();
       englishWord = englishWord.toLowerCase();
       vocabBank[japananeseWord]= englishWord;
-      japaneseTranslationArray.push(vocabulary[i]);
-      englishTranslationArray.push(vocabulary[i+1]);
+      japaneseTranslationArray.push(japananeseWord);
+      englishTranslationArray.push(englishWord);
     };
   });
 
@@ -40,25 +40,18 @@ fs.readFile('lesson-3.txt', 'utf8', function (err, file) {
   let questionChoicesObject = {};
   let choices = [];
 
+  function excludeCorrectAnswer(array, correctAnswer){
+    return array.filter(item => item !== correctAnswer);
+    }
+
   function nextWord(){
     if(currentIndex < 20) {
     // if(currentIndex < japaneseTranslationArray.length) { un-comment this line and comment out the one above for a longer test
       let currentWordEnglish = vocabBank[japaneseTranslationArray[currentIndex]]
       console.log(currentIndex+1,'. ',japaneseTranslationArray[currentIndex], ': \n');
       shuffle(englishTranslationArray);
-      incorrectQuestionPool = englishTranslationArray;
-      choicesAreUnqiue = false;
-      while(choicesAreUnqiue == false){
-        choices = [englishTranslationArray[0],englishTranslationArray[1],englishTranslationArray[2]]
-        if(choices.includes(currentWordEnglish)){
-          choices = []
-          shuffle(englishTranslationArray);
-        } else {
-          choicesAreUnqiue = true;
-        }
-        choices.push(currentWordEnglish)
-      } 
-      // choices = [englishTranslationArray[0],englishTranslationArray[1],englishTranslationArray[2],currentWordEnglish];
+      let incorrectAnswerPool = excludeCorrectAnswer(englishTranslationArray, currentWordEnglish);
+      choices = [incorrectAnswerPool[0],incorrectAnswerPool[1],incorrectAnswerPool[2],currentWordEnglish];
       shuffle(choices);
       questionChoicesObject['a'] = choices[0].trim().toLowerCase();
       questionChoicesObject['b'] = choices[1].trim().toLowerCase();
